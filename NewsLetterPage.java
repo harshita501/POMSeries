01096f7a7fb435b603ef20476aa8e6371faa1bfd
harshita501 <59357173+harshita501@users.noreply.github.com>
2021-03-13 08:04:54 -0700

    Create NewsLetterPage.java
    addedNewsLetterPage class

---
+++ NewsLetterPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class NewsLetterPage {
+	private By letter = By.id("letter");
+	
+	public NewsLetterPage() {
+		System.out.println("cart page const...");
+	}
+
+}
